430d5e63d036271f3f29d1a552752a5cab8b7ae5
Blackcypher <ulrich.sadou@hotmail.com>
2018-09-16 19:06:19 +0200

    add appbar with icons in homeActibvity

--- app/src/main/java/com/jupiter/ulric/blindtestfun/HomeActivity.java
+++ app/src/main/java/com/jupiter/ulric/blindtestfun/HomeActivity.java
@@ -1,69 +1,32 @@
 package com.jupiter.ulric.blindtestfun;
 
-
-import android.content.Intent;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ProgressBar;
+import android.view.animation.Animation;
+import android.widget.GridLayout;
+import android.widget.LinearLayout;
+
+import com.jupiter.ulric.blindtestfun.backgrounds.GraphicsView;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private LinearLayout lin_title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbar.setTitle("       Fun Blind Test");
         setSupportActionBar(toolbar);
-        toolbar.inflateMenu(R.menu.menu);
-        toolbar.setOnMenuItemClickListener(
-                new Toolbar.OnMenuItemClickListener() {
-                    @Override
-                    public boolean onMenuItemClick(MenuItem item) {
-                        System.out.println(" id === " + item.getItemId());
-                        return onOptionsItemSelected(item);
-                    }
-                });
+        getSupportActionBar().setTitle("");
 
-        Button fab = (Button) findViewById(R.id.actionRandom);
+        lin_title = (LinearLayout)findViewById(R.id.lin_title);
+        lin_title.addView(new GraphicsView(this));
 
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(HomeActivity.this, RandomPlayActivity.class);
-                startActivity(i);
-            }
-        });
-
-    }
-
-    private void startProfilActivity() {
-        Intent i = new Intent(HomeActivity.this, ProfilActivity.class);
-        startActivity(i);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        System.out.println(" id === " + item.getItemId());
-        if (item.getItemId() == R.id.miProfile) {
-            startProfilActivity();
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
-
---
+++ app/src/main/java/com/jupiter/ulric/blindtestfun/backgrounds/GraphicsView.java
@@ -0,0 +1,66 @@
+package com.jupiter.ulric.blindtestfun.backgrounds;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Typeface;
+import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+
+/**
+ * Created by ulric on 16/09/2018.
+ */
+
+
+public class GraphicsView extends View {
+    private static final String AnimatedTxt = "Fun Blind Test";
+    public Context context;
+
+    private Animation rotateAnim;
+
+    public GraphicsView(Context context) {
+        super(context);
+        this.context = context;
+    }
+
+    private void createAnim(Canvas canvas) {
+        rotateAnim = new RotateAnimation(-5, 5, canvas.getWidth() / 2, canvas.getHeight() / 2);
+        rotateAnim.setRepeatMode(Animation.REVERSE);
+        rotateAnim.setRepeatCount(Animation.INFINITE);
+        rotateAnim.setDuration(5000L);
+        rotateAnim.setInterpolator(new AccelerateDecelerateInterpolator());
+
+        startAnimation(rotateAnim);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        // creates the animation the first time
+        if (rotateAnim == null) {
+            createAnim(canvas);
+        }
+
+        Path circle = new Path();
+
+        int centerX = 0;
+        int centerY = canvas.getHeight() / 2;
+        int r = Math.min(centerX, centerY);
+
+        circle.addCircle(centerX, centerY, r, Path.Direction.CW);
+        Paint paint = new Paint();
+        paint.setColor(Color.WHITE);
+        Typeface chops = Typeface.createFromAsset(context.getAssets(), "titleStyle.ttf");
+        paint.setTypeface(chops);
+        paint.setTextSize(45);
+        paint.setAntiAlias(true);
+
+        canvas.drawText(AnimatedTxt, centerX, centerY, paint);
+        //canvas.drawTextOnPath(AnimatedTxt, circle, 0, 25, paint);
+    }
+}
