ac5088602362820dde3b0bef378d3965501b1440
usadou <ulrich.sadou@capgemini.com>
2018-10-25 17:40:30 +0200

    add swipeMenuListView

--- app/src/main/java/com/jupiter/ulric/blindtestfun/HomeActivity.java
+++ app/src/main/java/com/jupiter/ulric/blindtestfun/HomeActivity.java
@@ -1,17 +1,24 @@
 package com.jupiter.ulric.blindtestfun;
 
+import android.content.Context;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Typeface;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.baoyz.swipemenulistview.SwipeMenu;
+import com.baoyz.swipemenulistview.SwipeMenuCreator;
+import com.baoyz.swipemenulistview.SwipeMenuItem;
+import com.baoyz.swipemenulistview.SwipeMenuListView;
 import com.jupiter.ulric.blindtestfun.backgrounds.rotatingBackground.RotatingImageView;
 import com.jupiter.ulric.blindtestfun.backgrounds.starAnimationView.StarAnimationView;
 import com.jupiter.ulric.blindtestfun.utils.Constants;
@@ -21,10 +28,14 @@ public class HomeActivity extends AppCompatActivity {
     //private LinearLayout lin_title;
     //private RelativeLayout layout_disc;
     //private FrameLayout frame_toolbar;
+    public static Context context;
+    public SwipeMenuListView listView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        context = this;
         setContentView(R.layout.activity_home);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -36,5 +47,68 @@ public class HomeActivity extends AppCompatActivity {
         TextView loginTextview = (TextView) findViewById(R.id.login);
         loginTextview.setTypeface(chops, Typeface.BOLD);
 
+        SwipeMenuCreator creator = new SwipeMenuCreator() {
+
+            @Override
+            public void create(SwipeMenu menu) {
+                // create "open" item
+                SwipeMenuItem openItem = new SwipeMenuItem(
+                        getApplicationContext());
+                // set item background
+                openItem.setBackground(new ColorDrawable(Color.rgb(0xC9, 0xC9,
+                        0xCE)));
+                // set item width
+                openItem.setWidth(dp2px(90));
+                // set item title
+                openItem.setTitle("Open");
+                // set item title fontsize
+                openItem.setTitleSize(18);
+                // set item title font color
+                openItem.setTitleColor(Color.WHITE);
+                // add to menu
+                menu.addMenuItem(openItem);
+
+                // create "delete" item
+                SwipeMenuItem deleteItem = new SwipeMenuItem(
+                        getApplicationContext());
+                // set item background
+                deleteItem.setBackground(new ColorDrawable(Color.rgb(0xF9,
+                        0x3F, 0x25)));
+                // set item width
+                deleteItem.setWidth(dp2px(90));
+                // set a icon
+                deleteItem.setIcon(R.drawable.ic_delete);
+                // add to menu
+                menu.addMenuItem(deleteItem);
+            }
+        };
+
+        listView = (SwipeMenuListView) findViewById(R.id.listView);
+        // set creator
+        listView.setMenuCreator(creator);
+        listView.setOnMenuItemClickListener(new SwipeMenuListView.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(int position, SwipeMenu menu, int index) {
+                switch (index) {
+                    case 0:
+                        // open
+                        break;
+                    case 1:
+                        // delete
+                        break;
+                }
+                // false : close the menu; true : not close the menu
+                return false;
+            }
+        });
+        // Right
+        listView.setSwipeDirection(SwipeMenuListView.DIRECTION_RIGHT);
+        // Left
+        listView.setSwipeDirection(SwipeMenuListView.DIRECTION_LEFT);
+
+    }
+    public static int dp2px(float dp) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dp * scale + 0.5f);
     }
 }
